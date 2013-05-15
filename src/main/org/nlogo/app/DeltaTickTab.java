bd4573189fc5ee56d57ad3d436acca4c53b7f16e
Aditi Wagh <aditiwagh@u.northwestern.edu>
2013-05-14 21:08:48 -0500

    Note for mutations changed

--- src/main/org/nlogo/app/DeltaTickTab.java
+++ src/main/org/nlogo/app/DeltaTickTab.java
@@ -758,7 +758,7 @@ public class DeltaTickTab
         if (putNoteWidget) {
             NoteWidget noteWidget = ((NoteWidget) interfacePanel.makeWidget("NOTE", false));
             WidgetWrapper widgetw = interfacePanel.addWidget(noteWidget, 0, 80, true, false);
-            String note = "Chance of mutation??";
+            String note = "Chance of mutation in";
             noteWidget.setBounds(0, 80, 20, 30);
             noteWidget.text_$eq(note);
             //noteWidget.validate();
