b0b7efd7888952613ec1a37b27da680ac7bdb468
Aditi Wagh (on Windows - office laptop) <aditiwagh@u.northwestern.edu>
2013-11-30 12:55:34 -0500

    Fixed condition drop bug
    Fixed bug that prevented trait block being dropped into a condition block
    if a behavior block was already present. This was due to the 1 block max
    limit in condition block. The limit was getting enforced on the trait block
    as well. Fixed by processing traitblock before checking for the limit.

--- src/main/org/nlogo/deltatick/dnd/ConditionDropTarget.java
+++ src/main/org/nlogo/deltatick/dnd/ConditionDropTarget.java
@@ -25,77 +25,80 @@ public class ConditionDropTarget
             throws IOException, UnsupportedFlavorException {
         Object o = transferable.getTransferData(CodeBlock.codeBlockFlavor);
         if (o instanceof Component) {
-            if (((ConditionBlock) block).canAddBlock()) {
-                if (o instanceof BehaviorBlock) {
-
-                    if (((ConditionBlock) block).addedRectPanel) {
-                        ((ConditionBlock) block).hideRectPanel();
-                        ((ConditionBlock) block).addedRectPanel = false;
-                    }
-
-                    addCodeBlock((BehaviorBlock) o);
-                    // Set this behavior's parent BreedBlock
-                    ((BehaviorBlock) o).setMyBreedBlock(((BreedBlock) block.getMyBreedBlock()));
-
-                    if (((BehaviorBlock) o).getIsMutate() == true) {
-                        ((BreedBlock) (block).getMyBreedBlock()).setReproduceUsed(true);
-                    }
-                    // If breed has traits, and any trait is applicable to this behavior block then show a the panel
-                    if (((BreedBlock) block.getMyBreedBlock()).numTraits() > 0) {
-                        boolean addPanel = false;
-                        for (String traitName : ((BehaviorBlock) o).getApplicableTraits()) {
-                            if (((BreedBlock) block.getMyBreedBlock()).hasTrait(traitName)) {
-                                addPanel = true;
-                            }
-                        }
-                        if (addPanel) {
-                            ((BehaviorBlock) o).removeBehaviorInput();
-                            ((BehaviorBlock) o).addTraitblockPanel();
-                        }
-                    }
-
-                    new BehaviorDropTarget((BehaviorBlock) o);
-                    return true;
+            // Check if behavior or condition block can be added
+            if ((o instanceof ConditionBlock) ||
+                    (o instanceof BehaviorBlock)) {
+                if (!cBlock.canAddBlock()) {
+                    // Display error message
+                    String message = new String("Oops! You can only add one block here.");
+                    JOptionPane.showMessageDialog(null, message, "Oops!", JOptionPane.INFORMATION_MESSAGE);
+                    return false;
                 }
-                else if (o instanceof ConditionBlock) {
-
-                    if (((ConditionBlock) block).addedRectPanel) {
-                        ((ConditionBlock) block).hideRectPanel();
-                        ((ConditionBlock) block).addedRectPanel = false;
-                    }
-
-                    addCodeBlock((ConditionBlock) o);
-                    new ConditionDropTarget((ConditionBlock) o);
-                    return true;
-                }
-                else if (o instanceof PatchBlock) {
-                    addCodeBlock((PatchBlock) o);
-                    //new ConditionDropTarget((PatchBlock) o);
-                    return true;
-                }
-                else if (o instanceof TraitBlockNew) {
+            }
+            if (o instanceof TraitBlockNew) {
+                if (cBlock.isTraitApplicable(((TraitBlockNew)o).getTraitName())) {
                     addCodeBlock((TraitBlockNew) o);
                     cBlock.setTrait(((TraitBlockNew) o).getTraitName());
                     cBlock.removeBehaviorInput();
                     ((TraitBlockNew) o).hideRemoveButton();
-
                     return true;
                 }
             }
-            else {
-                // Display error message
-                String message = new String("Oops! You can only add one block here.");
-                JOptionPane.showMessageDialog(null, message, "Oops!", JOptionPane.INFORMATION_MESSAGE);
-                return false;
+            else if (o instanceof BehaviorBlock) {
+
+                if (((ConditionBlock) block).addedRectPanel) {
+                    ((ConditionBlock) block).hideRectPanel();
+                    ((ConditionBlock) block).addedRectPanel = false;
+                }
+
+                addCodeBlock((BehaviorBlock) o);
+                // Set this behavior's parent BreedBlock
+                ((BehaviorBlock) o).setMyBreedBlock(((BreedBlock) block.getMyBreedBlock()));
+
+                if (((BehaviorBlock) o).getIsMutate() == true) {
+                    ((BreedBlock) (block).getMyBreedBlock()).setReproduceUsed(true);
+                }
+                // If breed has traits, and any trait is applicable to this behavior block then show a the panel
+                if (((BreedBlock) block.getMyBreedBlock()).numTraits() > 0) {
+                    boolean addPanel = false;
+                    for (String traitName : ((BehaviorBlock) o).getApplicableTraits()) {
+                        if (((BreedBlock) block.getMyBreedBlock()).hasTrait(traitName)) {
+                            addPanel = true;
+                        }
+                    }
+                    if (addPanel) {
+                        ((BehaviorBlock) o).removeBehaviorInput();
+                        ((BehaviorBlock) o).addTraitblockPanel();
+                    }
+                }
+
+                new BehaviorDropTarget((BehaviorBlock) o);
+                return true;
+            }
+            else if (o instanceof ConditionBlock) {
+
+                if (((ConditionBlock) block).addedRectPanel) {
+                    ((ConditionBlock) block).hideRectPanel();
+                    ((ConditionBlock) block).addedRectPanel = false;
+                }
+
+                addCodeBlock((ConditionBlock) o);
+                new ConditionDropTarget((ConditionBlock) o);
+                return true;
+            }
+            else if (o instanceof PatchBlock) {
+                addCodeBlock((PatchBlock) o);
+                //new ConditionDropTarget((PatchBlock) o);
+                return true;
             }
             //return false; - commented out by A. (nov 27)
         }
         return false;
     }
     public void dragEnter(DropTargetDragEvent dtde) {
-        if (((ConditionBlock) block).canAddBlock()) {
-            if ((dtde.isDataFlavorSupported(CodeBlock.behaviorBlockFlavor)) ||
-                    (dtde.isDataFlavorSupported(CodeBlock.conditionBlockFlavor))) {
+        if ((dtde.isDataFlavorSupported(CodeBlock.behaviorBlockFlavor)) ||
+                (dtde.isDataFlavorSupported(CodeBlock.conditionBlockFlavor))) {
+            if (((ConditionBlock) block).canAddBlock()) {
                 ((ConditionBlock) block).showRectPanel();
                 ((ConditionBlock) block).addedRectPanel = true;
             }
@@ -103,11 +106,9 @@ public class ConditionDropTarget
     }
 
     public void dragExit(DropTargetEvent dte) {
-        if (((ConditionBlock) block).canAddBlock()) {
-            if (((ConditionBlock) block).addedRectPanel) {
-                ((ConditionBlock) block).hideRectPanel();
-                ((ConditionBlock) block).addedRectPanel = false;
-            }
+        if (((ConditionBlock) block).addedRectPanel) {
+            ((ConditionBlock) block).hideRectPanel();
+            ((ConditionBlock) block).addedRectPanel = false;
         }
     }
 
